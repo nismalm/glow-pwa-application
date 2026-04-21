0e8c63f4964cbd4f785b023ca013c1df1e7ec2cf
nismalm <nismalm6@gmail.com>
2026-04-21 11:20:21 +0530

    Firebase change

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,8 @@
 import {
   signInAnonymously,
   signInWithPopup,
+  signInWithRedirect,
+  getRedirectResult,
   GoogleAuthProvider,
   signOut as firebaseSignOut,
 } from 'firebase/auth'
@@ -8,6 +10,15 @@ import { auth } from './firebase'
 
 const googleProvider = new GoogleAuthProvider()
 
+const isMobile = /Mobi|Android|iPhone|iPad|iPod/i.test(navigator.userAgent)
+
 export const signInAnon = () => signInAnonymously(auth)
-export const signInGoogle = () => signInWithPopup(auth, googleProvider)
+
+export const signInGoogle = () =>
+  isMobile
+    ? signInWithRedirect(auth, googleProvider)
+    : signInWithPopup(auth, googleProvider)
+
+export const handleRedirectResult = () => getRedirectResult(auth)
+
 export const signOutUser = () => firebaseSignOut(auth)
